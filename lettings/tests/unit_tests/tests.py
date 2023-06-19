6c53635c52428aea6b1da020b8e0e516df329702
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-06-19 14:03:08 +0200

    ﻿App: lettings - Structuration: Déplacement de test.py vers  ./tests/unit_tests/ - Structuration: Les tests unitaires scindé par vue. - Ajout: LettingsIndexTest     - Ajout: Test de présence du title.     - Ajout: Test de la template utilisé.     - Factorisation: De la response GET vers la vue dans setUp().     - Factorisation: D'un db de test (non sujette à modification) dans setUpTsetData(). - Ajout: LettingsLettingTest     - Ajout: Test du codeHTML de la response.     - Ajout: Test de présence du title.     - Ajout: Test de la template utilisé.

---
+++ lettings/tests/unit_tests/tests.py
@@ -0,0 +1,81 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from lettings.models import Address, Letting
+
+
+class LettingsIndexTest(TestCase):
+    """Tests de la view index."""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Configuration commune et initiale pour la class.
+
+        La DB de test doit rester non modifié par les tests, car appeler
+        la méthode n'est appelé qu'une seule fois pour l'ensemble des
+        tests de la class.
+        """
+        cls.address = Address.objects.create(
+            number=4, street="my street", city="in city", state="room",
+            zip_code=12345, country_iso_code="RSM")
+        cls.letting = Letting.objects.create(title="A letting", address_id=1)
+
+    def setUp(self):
+        """Configuration commune pour à chaque test.
+
+        Les class de tests étant scindé par view, la response est
+        communes pour tous les tests de la class.
+        L'appel se fait avant chacun des tests.
+        """
+        self.response = self.client.get(reverse("lettings:index"))
+
+    def test_200_status_code(self):
+        return self.assertEqual(self.response.status_code, 200)
+
+    def test_title_in_response(self):
+        expected_title = b"<title>Lettings</title>"
+        return self.assertContains(self.response, expected_title,
+                                   status_code=200)
+
+    def test_template_used(self):
+        expected_template = "lettings/index.html"
+        return self.assertTemplateUsed(self.response, expected_template)
+
+
+class LettingsLettingTest(TestCase):
+    """Tests de la view letting."""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Configuration commune et initiale pour la class.
+
+        La DB de test doit rester non modifié par les tests, car appeler
+        la méthode n'est appelé qu'une seule fois pour l'ensemble des
+        tests de la class.
+        """
+        cls.address = Address.objects.create(
+            number=4, street="my street", city="in city", state="room",
+            zip_code=12345, country_iso_code="RSM")
+        cls.letting = Letting.objects.create(title="A letting", address_id=1)
+
+    def setUp(self):
+        """Configuration commune pour à chaque test.
+
+        Les class de tests étant scindé par view, la response est
+        communes pour tous les tests de la class.
+        L'appel se fait avant chacun des tests.
+        """
+        self.response = self.client.get(reverse("lettings:letting",
+                                                args=[self.letting.id]))
+
+    def test_200_status_code(self):
+        return self.assertEqual(self.response.status_code, 200)
+
+    def test_title_in_response(self):
+        expected_title = f"<title>{self.letting.title}</title>".encode()
+        return self.assertContains(self.response, expected_title,
+                                   status_code=200)
+
+    def test_template_used(self):
+        expected_template = "lettings/letting.html"
+        return self.assertTemplateUsed(self.response, expected_template)
