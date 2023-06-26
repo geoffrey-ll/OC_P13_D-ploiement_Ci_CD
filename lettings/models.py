fd1140e833ea5e5225aa3901e90f4f6f3a7b6a94
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-06-26 16:22:34 +0200

    Change verbose_name in Adress model to verobose_name_plural

--- lettings/models.py
+++ lettings/models.py
@@ -14,7 +14,7 @@ class Address(models.Model):
         max_length=3, validators=[MinLengthValidator(3)])
 
     class Meta:
-        verbose_name = "Addresses"
+        verbose_name_plural = "Addresses"
 
     def __str__(self):
         return f'{self.number} {self.street}'
