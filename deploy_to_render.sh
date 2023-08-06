36d219459e134b0a49de75c9337728e863e04763
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-06 20:27:10 +0200

    012

---
+++ deploy_to_render.sh
@@ -0,0 +1,14 @@
+
+
+
+
+curl --request POST \
+     --url "https://api.render.com/v1/services/srv-cj7svp4l975s73e6i6v0/deploys" \
+     --header "Accept: application/json" \
+     --header "Authorization: Bearer rnd_0iWsxmnOZvGWwJl6b8PFGOcaPobm" \
+     --header "Content-Type: application/json" \
+     --write-out '%{http_code}' \
+     --silent \
+     --output /dev/null
+
+
