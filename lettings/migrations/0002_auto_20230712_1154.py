1e29577ea002506486bfaee351038625dfe588d8
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-06 19:18:42 +0200

    006

---
+++ lettings/migrations/0002_auto_20230712_1154.py
@@ -0,0 +1,44 @@
+from django.apps import apps as global_apps
+from django.db import migrations
+
+
+def move_data_from_old_lettings_to_new_lettings(apps, schema_editor):
+    try:
+        OldLetting = apps.get_model("oc_lettings_site", "Letting")
+    except LookupError:
+        return
+    attributes = ["number", "street", "city",
+                  "state", "zip_code", "country_iso_code"]
+    NewAddress = apps.get_model("lettings", "Address")
+    NewLetting = apps.get_model("lettings", "Letting")
+
+    for old_letting in OldLetting.objects.all():
+        old_title = old_letting.title
+        old_address = old_letting.address
+        dict_old_address = {
+            attribute: getattr(old_address, attribute)
+            for attribute in attributes
+        }
+        new_address, bool_status = \
+            NewAddress.objects.get_or_create(**dict_old_address)
+        try:
+            NewLetting.objects.create(title=old_title, address=new_address)
+        except LookupError:
+            continue
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lettings', '0001_initial'),
+    ]
+
+    if global_apps.is_installed("oc_lettings_site"):
+        dependencies.append(("oc_lettings_site", "0001_initial"))
+
+    operations = [
+        migrations.RunPython(move_data_from_old_lettings_to_new_lettings,
+                             migrations.RunPython.noop),
+        migrations.RunSQL([("DROP TABLE oc_lettings_site_address;"),
+                           ("DROP TABLE oc_lettings_site_letting;")])
+    ]
---
+++ profiles/migrations/0002_auto_20230712_1154.py
@@ -0,0 +1,34 @@
+from django.apps import apps as global_apps
+from django.db import migrations
+
+
+def move_data_from_old_profiles_to_new_profiles(apps, schema_editor):
+    try:
+        OldProfiles = apps.get_model("oc_lettings_site", "Profile")
+    except LookupError:
+        return
+    NewProfile = apps.get_model("profiles", "Profile")
+
+    try:
+        return NewProfile.objects.bulk_create(
+            NewProfile(user=old_object.user,
+                       favorite_city=old_object.favorite_city)
+            for old_object in OldProfiles.objects.all())
+    except LookupError:
+        return
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profiles', '0001_initial'),
+    ]
+
+    if global_apps.is_installed("oc_lettings_site"):
+        dependencies.append(("oc_lettings_site", "0001_initial"))
+
+    operations = [
+        migrations.RunPython(move_data_from_old_profiles_to_new_profiles,
+                             migrations.RunPython.noop),
+        migrations.RunSQL("DROP TABLE oc_lettings_site_profile;")
+    ]
