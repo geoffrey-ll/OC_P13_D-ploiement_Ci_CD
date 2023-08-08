6a07bd0da14ff1587e1e1a2f4f2546c33b05c12f
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-08 12:28:02 +0200

    065

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,8 +15,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-if DEBUG is True:
-    ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "*"]
 
 DJANGO_APPS = [
     'django.contrib.admin',
