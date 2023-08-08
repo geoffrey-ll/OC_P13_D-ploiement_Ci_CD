eb6ca6a08ad6b32eeb49c6ed70e07da23cc1bea2
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-08 09:56:29 +0200

    048

--- deploy_to_render.sh
+++ deploy_to_render.sh
@@ -1,9 +1,9 @@
 # Requête curl pour le déploiement CI
 
 curl --request POST \
-     --url 'https://api.render.com/v1/services/'${RENDER_SERVICE_ID}'/deploys' \
+     --url 'https://api.render.com/v1/services/'${RENDER_SERVICE_ID} \
      --header "Accept: application/json" \
-     --header 'Authorization: Bearer '${RENDER_API_KEY}'' \
+     --header 'Authorization: Bearer '${RENDER_API_KEY} \
      --header "Content-Type: application/json" \
      --write-out '%{http_code}\n\n %{url}' \
      --silent \
