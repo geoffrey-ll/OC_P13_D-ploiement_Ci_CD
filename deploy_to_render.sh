8c987fdda1176ce845b4917dd2c4707573c93fbb
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-08 09:28:42 +0200

    038

--- deploy_to_render.sh
+++ deploy_to_render.sh
@@ -1,9 +1,9 @@
 # Requête curl pour le déploiement CI
 
 curl --request POST \
-     --url "https://api.render.com/v1/services/RENDER_SERVICE_ID/deploys" \
+     --url "https://api.render.com/v1/services/'%{RENDER_SERVICE_ID}'/deploys" \
      --header "Accept: application/json" \
-     --header "Authorization: Bearer RENDER_API_KEY" \
+     --header "Authorization: Bearer '%{RENDER_API_KEY}'" \
      --header "Content-Type: application/json" \
      --write-out '%{http_code}' \
      --silent \
@@ -11,9 +11,9 @@ curl --request POST \
 
 
 - |
-  curl --fail --output "/dev/null" --silent --show-error --write-out "HTTP response: ${http_code}\n\n" \
-    --data "{\"tag_name\": \"${CI_COMMIT_TAG}\", \"name\": \"${CI_PROJECT_NAME} ${CI_COMMIT_TAG}\", \"description\": \"${CI_COMMIT_TAG_MESSAGE:-No release notes.}\"}" \
-    --header "Content-Type: application/json" \
-    --header "Private-Token: ${CI_PRIVATE_TOKEN}" \
-    --request POST \
-    "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/releases"
+#  curl --fail --output "/dev/null" --silent --show-error --write-out "HTTP response: ${http_code}\n\n" \
+#    --data "{\"tag_name\": \"${CI_COMMIT_TAG}\", \"name\": \"${CI_PROJECT_NAME} ${CI_COMMIT_TAG}\", \"description\": \"${CI_COMMIT_TAG_MESSAGE:-No release notes.}\"}" \
+#    --header "Content-Type: application/json" \
+#    --header "Private-Token: ${CI_PRIVATE_TOKEN}" \
+#    --request POST \
+#    "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/releases"
