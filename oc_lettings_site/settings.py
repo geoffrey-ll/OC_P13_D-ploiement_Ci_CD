655f9ce16a4975615e94716b5e93b80c676b544a
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-11 14:38:40 +0200

    test 001

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,6 +17,14 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
+# DEBUG = os.getenv(f"{os.getenv('DEBUG', default='False')}")
+# if DEBUG is T
+
+# try:
+#   DEBUG = os.getenv(f"{os.getenv('DEBUG')}")
+# except:
+#   DEBUG = config("DEBUG", cast=bool)
+
 if DEBUG is True:
     ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 elif DEBUG is False:
