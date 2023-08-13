82786a29942b153babdcb83f9462fe0ecd63f5ae
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-13 22:51:17 +0200

    pb tag images: les deux push après avoir delete l'image :latest sur hub-docker

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,19 +5,6 @@ from decouple import config
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-# Ajout d'un commentaire pour la nouvelle image
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
