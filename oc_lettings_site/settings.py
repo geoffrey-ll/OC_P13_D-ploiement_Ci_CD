ede52306dbda95d4441ddf25f25f53390af611b8
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2023-08-13 22:33:00 +0200

    pb tag images: avec modification ailleurs que dans .gitlab-ci.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -5,6 +5,14 @@ from decouple import config
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
+# Ajout d'un commentaire pour la nouvelle image
+
+
+
+
+
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
